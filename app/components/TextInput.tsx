0af4483c0804ab133c8ca19d31fc79fe83fb6fe7
Yusuf <yhajiali7@gmail.com>
2024-04-19 09:16:45 +0100

    Initialise LogoAI Landing

---
+++ app/components/TextInput.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = {
+  label: string;
+  placeholder: string;
+};
+
+const TextInput = (props: Props) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label htmlFor="logo-name">{props.label}</label>
+      <input
+        type="text"
+        className="outline-none bg-transparent border-x-0 border-t-0 border-2 border-gray-600 w-full py-2 focus:border-blue-600 transition-all duration-300"
+        id="logo-name"
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
