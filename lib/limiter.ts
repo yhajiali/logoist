cd4c7fb96374380aaa4512877bfb86d4c274d0a4
sabirov07 <shaxzod4242424@gmail.com>
2024-05-14 23:57:11 +0500

    Apply Rate Limiting 🔥

---
+++ lib/limiter.ts
@@ -0,0 +1,14 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  });
+
+export const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.slidingWindow(3, "86400 s"), // 3 requests per day 86400 = one day
+    ephemeralCache: new Map(),
+    analytics: true,
+  });
